e7a382774838f6114d307e469cdf8850e5db3d1d
Dmytro <dmitry.yurin2020@gmail.com>
2024-01-24 17:15:13 +0200

    tailwindcss setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1>🍕 PizzaStack - найкраща IT-піцерія України</h1>
+    <h1 className="text-3xl font-bold underline">🍕 PizzaStack - найкраща IT-піцерія України</h1>
   );
 }
 
