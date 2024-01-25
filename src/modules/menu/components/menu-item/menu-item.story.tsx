5eb484a5e57b61511ba0d4757d2aedaf1204d8a3
Dmytro <dmitry.yurin2020@gmail.com>
2024-01-25 12:24:25 +0200

    create separate components folder for menu module

--- src/modules/menu/components/menu-item/menu-item.story.tsx
+++ src/modules/menu/components/menu-item/menu-item.story.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import MenuItem from "./menu-item.component";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+
 const meta = {
   title: "Menu/Menu Item",
   component: MenuItem,
@@ -10,7 +10,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+
 export const View: Story = {
   args: {
     imagePath: "/assets/pizza/manhattan.jpeg",
--- src/modules/menu/components/menu-list/menu-list.story.tsx
+++ src/modules/menu/components/menu-list/menu-list.story.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import MenuList from "./menu-list.component";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+
 const meta = {
   title: "Menu/Menu List",
   component: MenuList,
@@ -10,5 +10,5 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+
 export const View: Story = {};
